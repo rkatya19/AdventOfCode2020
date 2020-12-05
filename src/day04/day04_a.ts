0423a8d8f83450ea965f1bf53412b7bc7ddfcb77
Katya Rebholz <rkatya19@vt.edu>
2020-12-04 19:08:14 -0500

    remove prints

--- src/day04/day04_a.ts
+++ src/day04/day04_a.ts
@@ -4,14 +4,11 @@ const input = await getInput(4);
 const grouped = input.replaceAll(' ', '\n');
 const rows = grouped.split('\n');
 
-// console.log(rows)
-
 var numValid = 0
 var fieldDictionary: Map<string, number> = new Map();
 for (let row of rows) {
 
     if (row == '') {
-        // console.log(fieldDictionary)
         if ((fieldDictionary.size == 8) || (fieldDictionary.size == 7 && !fieldDictionary.has('cid'))) {
             numValid++
         }
